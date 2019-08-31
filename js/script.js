0fb88a4940b23d4613c0db29452bb01b8b68bb35
Grzegorz Sienkiewicz <grzegorz.sienkiewicz@eu.jll.com>
2019-08-31 10:22:43 +0200

    Add example functions

---
+++ js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// *** TEST Button ***
+// function titleClickHandler() {
+//   const links = document.querySelectorAll(".titles a");
+//   console.log(links);
+// }
+// const buttonTest = document.getElementById("button-test");
+// buttonTest.addEventListener("click", titleClickHandler);
+
+// *** Loop example ***
+// function titleClickHandler() {
+//   console.log("Link was clicked!");
+// }
+// const links = document.querySelectorAll(".titles a");
+// for (let link of links) {
+//   link.addEventListener("click", titleClickHandler);
+// }
+
+function titleClickHandler(event) {
+  console.log("Link was clicked!");
+  console.log(event);
+  /* remove class 'active' from all article links  */
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll(".titles a");
+
+for (let link of links) {
+  link.addEventListener("click", titleClickHandler);
+}
