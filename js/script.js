b3c2cff66ce8c7e88161aaf2562de20196949a03
Grzegorz Sienkiewicz <grzegorz.sienkiewicz@eu.jll.com>
2019-09-06 22:18:40 +0200

    Clear code, fix errors, code ready to continue work

--- js/script.js
+++ js/script.js
@@ -8,57 +8,38 @@ function titleClickHandler(event) {
   const clickedElement = this;
   console.log('Link was clicked!');
 
-  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
 
-  /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
-
-  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
 
-  /* [DONE] get 'href' attribute from the clicked link */
   const hrefAttribute = clickedElement.getAttribute('href');
-  // console.log(hrefAttribute);
-
-  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(hrefAttribute);
-
-  /* [DONE] add class 'active' to the correct article */
   targetArticle.classList.add('active');
 }
 
 function generateTitleLinks() {
-  /* [DONE] remove contents of titleList */
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
-
-  /* [DONE] for each article */
   const articles = document.querySelectorAll(optArticleSelector);
-
   let html = '';
 
   for (let article of articles) {
-    /* [DONE] get the article id */
-    /* [DONE] find the title element */
-    /* [DONE] get the title from the title element */
-
-    /* create HTML of the link */
+    const articleId = article.getAttribute('id');
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
     const linkHTML =
-      `<li><a href='#` +
-      `articleId` +
-      `><span>` +
-      `articleTitle` +
-      `</span></a></li>`;
-
-    /* [DONE] insert link into html variable */
+      '<li><a href="#' +
+      articleId +
+      '"><span>' +
+      articleTitle +
+      '</span></a></li>';
     html = html + linkHTML;
   }
 
